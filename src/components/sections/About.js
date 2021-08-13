9c48a2a8b0d506cabe177cd03299d3a3785498ed
Bruno Ramari <kbruskbrus@gmail.com>
2021-08-13 17:41:51 -0300

    animacion solo una vez

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -27,12 +27,17 @@ const useOnScreen = (options) => {
 
 const About = props => {
 
-    const [ref, visible] = useOnScreen({ threshold: 0.5 })
+    const [ref, visible] = useOnScreen({ threshold: 0.5 });
+    const [trigger, setTrigger] = React.useState(false);
+
+    React.useEffect(() => {
+        if (visible) setTrigger(true)
+    }, [visible])
 
     return (
         <div ref={ref} id={`${props.id}`} className="about">
             <div className="about__right">
-                <h1 className="about__title">{props.id}<div className={`about__title__bar ${visible ? "animate-border" : null}`}></div></h1>
+                <h1 className="about__title">{props.id}<div className={`about__title__bar ${trigger ? "animate-border" : null}`}></div></h1>
                 <div className="about__text">
 
                     <div>Somos una compañía joven con amplia experiencia en el sector consorcios, oficinas y grandes obras. Con miembros profesionales y capacitados para gestionar calidad y control sobre el personal, el trabajo y los productos utilizados.<br />
@@ -42,7 +47,7 @@ const About = props => {
 
                 </div>
             </div>
-            <div className={`about__image ${visible ? "image-opacity" : null}`}></div>
+            <div className={`about__image ${trigger ? "image-opacity" : null}`}></div>
         </div >
     )
 }
